c49d7fc178f89957f921530de154c66f603db007
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-05 23:37:51 +0600

    product model is completed

---
+++ static/js/cart.js
@@ -0,0 +1,43 @@
+
+var updateBtns = document.getElementsByClassName('update-cart');
+
+for (var i = 0; i<updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productID', productId, 'action',action);
+
+        console.log('user', user);
+        if (user === 'AnonymousUser'){
+            console.log('user not log in')
+        }
+        else {
+            updateUserOrder(productID,action)
+        }
+
+    })
+
+}
+
+
+function updateUserOrder(productID, action) {
+    console.log('User is logged in , sending data');
+
+    var url= '/update_item/';
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({'productID':productID, 'action':action})
+    })
+
+        .then((response) =>{
+            return response.json()
+        })
+        .then((data) =>{
+            console.log('data',data)
+        })
+
+
+}
--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from .models import *
 
+from  django.http import JsonResponse
 
 def store(request):
     products = Product.objects.all()
@@ -32,3 +33,6 @@ def checkout(request):
         order= {'get_cart_total':0, 'get_cart_items':0}
     context = {'items':items,'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse("Item was added", self=False)
